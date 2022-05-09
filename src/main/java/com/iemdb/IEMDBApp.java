f7785917eabc9cb12df359fbc5e8dc25db866e05
Arash <arash3908@gmail.com>
2022-05-09 13:41:32 +0430

    Fixed IEMDBApp.

--- src/main/java/com/iemdb/IEMDBApp.java
+++ src/main/java/com/iemdb/IEMDBApp.java
@@ -1,35 +1,15 @@
 package com.iemdb;
 
-import com.iemdb.data.ActorRepository;
-import com.iemdb.data.IemdbRepository;
-import com.iemdb.data.MovieRepository;
-import com.iemdb.model.Actor;
-import com.iemdb.model.Movie;
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-import io.swagger.models.auth.In;
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.sql.rowset.*;
-import java.sql.*;
-import java.util.ArrayList;
-
-import java.util.Map;
-import java.util.regex.*;
-
-import static java.util.Map.entry;
-
-//@SpringBootApplication
-//@ServletComponentScan
-//@EnableSwagger2
+@SpringBootApplication
+@ServletComponentScan
+@EnableSwagger2
 public class IEMDBApp {
 	public static void main(String[] args) {
-//		SpringApplication.run(IEMDBApp.class, args);
-
-		IemdbRepository iemdbRepository = new IemdbRepository();
-		ActorRepository actorRepository = new ActorRepository();
-		ArrayList<Movie> movies = actorRepository.getActorMovies(38);
+		SpringApplication.run(IEMDBApp.class, args);
 	}
 }
